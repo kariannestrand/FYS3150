1f8fd1876466c5efb3f32923a9ee20e030da2bbc
kariannestrand <kariannestrand@outlook.com>
2021-09-02 10:41:58 +0200

    karianne worked when she should not have

--- problem2.cpp
+++ problem2.cpp
@@ -2,11 +2,24 @@
 #include <iostream>
 #include <armadillo>
 
-double n = 10;
+using namespace arma;
+using namespace std;
+double n = 11; // length of array
 
 int main(){
-  arma::vec x = arma::linspace(0, 1, n);
-  arma::vec u = 1 - (1 - exp(-10))*x - exp(-10*x);
-  std::cout << x << u << "\n";
+  vec x = arma::linspace(0, 1, n); // defining array x in [0, 1]
+  vec u = 1 - (1 - exp(-10))*x - exp(-10*x); // calculating solution
+
+  // writing solution to file called problem2.txt
+  ofstream myfile;
+  myfile.open ("problem2.txt");
+  myfile << setw(15) << setprecision(8) << "x";
+  myfile << setw(15) << setprecision(8) << "u(x)" << endl;
+  for (int i = 0; i < n; i++){
+    myfile << setw(15) << setprecision(8) << x(i);
+    myfile << setw(15) << setprecision(8) << u(i) << endl; // formatting txt-file
+  }
+  myfile.close();
+
   return 0;
 }
