13547605c7b511a9f4c307a67391fe653f59c306
mariaoftedahl <mloftedahl@gmail.com>
2021-09-09 11:02:14 +0200

    trying to plot approx against exact sol

--- Project_1/problem2.py
+++ Project_1/problem2.py
@@ -1,17 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = []; u = []
+x_exact = []; u_exact = []
 with open("problem2.txt") as f:
     f.readline()
     for line in f.readlines():
         line = line.split()
-        x.append(float(line[0])); u.append(float(line[1]))
+        x_exact.append(float(line[0])); u_exact.append(float(line[1]))
+
+x10 = []; u10 = []
+with open("problem7.txt") as f:
+    f.readline()
+    for line in f.readlines():
+        line = line.split()
+        x10.append(float(line[0])); u10.append(float(line[1]))
+
+x100 = []; u100 = []
+with open("problem7.txt") as f:
+    f.readline()
+    for line in f.readlines():
+        line = line.split()
+        x100.append(float(line[0])); u100.append(float(line[1]))
+
+x1000 = []; u1000 = []
+with open("problem7.txt") as f:
+    f.readline()
+    for line in f.readlines():
+        line = line.split()
+        x1000.append(float(line[0])); u1000.append(float(line[1]))
 
 
-plt.plot(x,u, linestyle="", marker="o")
+plt.plot(x_exact, u_exact, linestyle="-", marker="o")
+plt.plot(x10, u10, inestyle="-", marker="o", color='red')
+plt.plot(x100, u100, inestyle="-", marker="o", color='green')
+plt.plot(x100, u100, inestyle="-", marker="o", color='yellow')
 plt.title('Plot of $u(x)=1-(1-e^{-10})x-e^{-10x}$')
 plt.xlabel('x-values')
 plt.ylabel('u(x)')
-plt.savefig("problem2.pdf")
+plt.savefig("problem7.pdf")
 plt.show()
--- Project_1/problem7.cpp
+++ Project_1/problem7.cpp
@@ -4,31 +4,33 @@
 
 using namespace arma;
 using namespace std;
-double n = 11; // length of array
+double n = 10; // length of array
 double h = 1/n; // step size
 
 int main(){
-    vec a = vec(n-1).fill(-1.); // defining a-vector
-    vec b = vec(n).fill(2.); // defining b-vector
-    vec c = vec(n-1).fill(-1.); // defining c-vector
+    vec a = vec(n-1).fill(-1.); // defining a-vector and filling with -1's
+    vec b = vec(n).fill(2.); // defining b-vector and filling with 2's
+    vec c = vec(n-1).fill(-1.); // defining c-vector and filling with -1's
     vec x = arma::linspace(0, 1, n); // defining array x in [0, 1]
     vec f = 100*exp(-10*x); // defining source term
-    vec g = h*h*f; // defining solution-vector g
-    vec v = vec(n);
+    vec g = h*h*f; // defining solution vector g
+    vec v = vec(n); // creating empty solution vector v
+    //Forward substitution
     for (int i = 1; i < n-1; i++){
         b(i) = b(i)-a(i)/b(i-1)*c(i-1);
-        g(i) = g(i)-a(i)*b(i-1)*g(i-1);
+        g(i) = g(i)-a(i)/b(i-1)*g(i-1);
     }
 
     v(n-1) = g(n-1)/b(n-1);
-
+    //Backwards substitution
     for (int i = n-2; i >= 0; i--){
       v(i) = (g(i)-c(i)*v(i+1))/b(i);
+      cout << v(i) << endl;
     }
-    cout << v << endl;
+    
 
     ofstream myfile;
-    myfile.open ("problem7.txt");
+    myfile.open ("problem7_10.txt");
     myfile << setw(15) << setprecision(8) << "x";
     myfile << setw(15) << setprecision(8) << "v" << endl;
     for (int i = 0; i < n; i++){
