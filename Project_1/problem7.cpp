9c70aadce6b7d4b115956602a8ca61345a094854
mariaoftedahl <mloftedahl@gmail.com>
2021-09-09 10:18:20 +0200

    code for problem 7 that gived output

--- Project_1/problem7.cpp
+++ Project_1/problem7.cpp
@@ -11,7 +11,7 @@ int main(){
     vec a = vec(n-1).fill(-1.); // defining a-vector
     vec b = vec(n).fill(2.); // defining b-vector
     vec c = vec(n-1).fill(-1.); // defining c-vector
-    vec x = arma::linspace(0+h, 1-h, n); // defining array x in [0, 1]
+    vec x = arma::linspace(0, 1, n); // defining array x in [0, 1]
     vec f = 100*exp(-10*x); // defining source term
     vec g = h*h*f; // defining solution-vector g
     vec v = vec(n);
@@ -20,17 +20,12 @@ int main(){
         g(i) = g(i)-a(i)*b(i-1)*g(i-1);
     }
 
-
-    //cout << g << endl;
-    //cout << b << endl;
     v(n-1) = g(n-1)/b(n-1);
-    //cout << v << endl;
+
     for (int i = n-2; i >= 0; i--){
       v(i) = (g(i)-c(i)*v(i+1))/b(i);
     }
     cout << v << endl;
-    //cout << b << endl;
-    //cout << g << endl;
 
     ofstream myfile;
     myfile.open ("problem7.txt");
