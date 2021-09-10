1d72d505c15ed1c0485eb23d0192848170544e5a
mariaoftedahl <mloftedahl@gmail.com>
2021-09-10 12:07:02 +0200

    made filereader

--- Project_1/main.py
+++ Project_1/main.py
@@ -2,58 +2,96 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-#n_list = [10, 100, 1000, 10000]
+n_list = [10, 100, 1000, 10000]
 
-#def file_reader(n_list):
-    #data = np.zeros((len(4), 2)) 
-    #for i in range(len(n_list)):
-        #v = np.zeros(n_list[i]+1)
-        #filename = "problem7_" + str(n_list[i]) + ".txt"
-        #v = 
+def file_reader(filename):
+    with open(filename, "r") as infile:
+        labels = infile.readline().split()
+        num_lists = len(labels)
+        lines = infile.readlines()
+        data = {key : [] for key in labels}
+        for line in lines:
+            vals = line.split()
+            for i in range(num_lists):
+                data[labels[i]].append(float(vals[i]))
+    return data
 
-x_exact = []; u_exact = []
-with open("problem2.txt") as f:
-    f.readline()
-    for line in f.readlines():
-        line = line.split()
-        x_exact.append(float(line[0])); u_exact.append(float(line[1]))
+exact_list = []
+approx_list = []
 
-x10 = []; u10 = []
-with open("problem7_10.txt") as f:
-    f.readline()
-    for line in f.readlines():
-        line = line.split()
-        x10.append(float(line[0])); u10.append(float(line[1]))
+for i in range(len(n_list)):
+    filename = "exact_" + str(n_list[i]) + ".txt"
 
-x100 = []; u100 = []
-with open("problem7_100.txt") as f:
-    f.readline()
-    for line in f.readlines():
-        line = line.split()
-        x100.append(float(line[0])); u100.append(float(line[1]))
+    data = file_reader(filename)
+    labels = data.keys()
+    data = {key : np.array(data[key]) for key in labels}
+    exact_list.append(data)
 
-x1000 = []; u1000 = []
-with open("problem7_1000.txt") as f:
-    f.readline()
-    for line in f.readlines():
-        line = line.split()
-        x1000.append(float(line[0])); u1000.append(float(line[1]))
+for i in range(len(n_list)):
+    filename = "approx_" + str(n_list[i]) + ".txt"
 
-x10000 = []; u10000 = []
-with open("problem7_10000.txt") as f:
-    f.readline()
-    for line in f.readlines():
-        line = line.split()
-        x10000.append(float(line[0])); u10000.append(float(line[1]))
+    data = file_reader(filename)
+    labels = data.keys()
+    data = {key : np.array(data[key]) for key in labels}
+    approx_list.append(data)
 
 
-plt.plot(x_exact, u_exact, linestyle="-", marker="o", color='blue')
-plt.plot(x10, u10, linestyle="-", marker="o", color='red')
-plt.plot(x100, u100, linestyle="-", color='green')
-plt.plot(x1000, u1000, linestyle="-", color='yellow')
-plt.plot(x10000, u10000, linestyle="-", color='brown')
+exact_10 = exact_list[0]
+labels_10 = exact_10.keys()
+x_exact_10 = exact_10[labels_10[0]]
+u_10 = exact_10[labels_10[1]]
+
+exact_100 = exact_list[1]
+exact_100 = exact_100.keys()
+x_exact_100 = exact_100[labels_100[0]]
+u_100 = exact_100[labels_100[1]]
+
+exact_1000 = exact_list[2]
+exact_1000 = exact_100.keys()
+x_exact_1000 = exact_10[labels_1000[0]]
+u_1000 = exact_10[labels_1000[1]]
+
+approx_10000 = approx_list[3]
+labels_10000 = approx_10000.keys()
+x_approx_10000 = approx_10000[labels_10000[0]]
+u_10000 = approx_10000[labels_10000[1]]
+
+approx_10 = approx_list[0]
+labels_10 = approx_10.keys()
+x_approx_10 = approx_10[labels_10[0]]
+v_10 = approx_10[labels_10[1]]
+
+approx_100 = approx_list[1]
+labels_100 = approx_100.keys()
+x_approx_100 = approx_10[labels_100[0]]
+v_100 = approx_10[labels_100[1]]
+
+approx_1000 = approx_list[2]
+labels_1000 = approx_100.keys()
+x_approx_1000 = approx_10[labels_100[0]]
+v_1000 = approx_10[labels_100[1]]
+
+approx_10000 = approx_list[3]
+labels_10000 = approx_10000.keys()
+x_approx_10000 = approx_10000[labels_10000[0]]
+v_10000 = approx_10000[labels_10000[1]]
+
+
+plt.figure()
+plt.plot(x_exact_10, u_10, linestyle="-", marker="o", color='blue')
 plt.title('Plot of $u(x)=1-(1-e^{-10})x-e^{-10x}$')
 plt.xlabel('x-values')
 plt.ylabel('u(x)')
-plt.savefig("problem7.pdf")
-plt.show()
+plt.savefig("comparison.pdf")
+
+plt.figure()
+plt.plot(x_exact_10, u_10, linestyle="-", marker="o", color='blue')
+plt.plot(x_approx_10, v_10, linestyle="-", marker="o", color='red')
+plt.plot(x_approx_100, v_100, linestyle="-", color='green')
+plt.plot(x_approx_1000, v_1000, linestyle="-", color='yellow')
+plt.plot(x_approx_10000, v_10000, linestyle="-", color='brown')
+plt.title('Comparison of exact and approximated solution')
+plt.xlabel('x-values')
+plt.ylabel('u(x)/v(x)')
+plt.savefig("comparison.pdf")
+#plt.show()
