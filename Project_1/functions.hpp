cbaf2356d63717f9eeaaee21f9b12134033f6cb3
kariannestrand <kariannestrand@outlook.com>
2021-09-12 12:39:38 +0200

    deleted all txt-files and added special algorithm

--- Project_1/functions.hpp
+++ Project_1/functions.hpp
@@ -7,9 +7,15 @@
 
 // Declearing functions
 arma::vec exact(arma::vec x);
-void forward(arma::vec a, arma::vec *b, arma::vec c, arma::vec *g, int n);
-void backward(arma::vec b, arma::vec c, arma::vec g, arma::vec *v, int n);
+
+void forward_gen(arma::vec a, arma::vec *b, arma::vec c, arma::vec *g, int n);
+void backward_gen(arma::vec b, arma::vec c, arma::vec g, arma::vec *v, int n);
 void gauss_elim_gen(arma::vec a, arma::vec b, arma::vec c, arma::vec g, arma::vec *v, int n);
+
+void forward_spec(arma::vec a, arma::vec *b, arma::vec c, arma::vec *g, int n);
+void backward_spec(arma::vec b, arma::vec c, arma::vec g, arma::vec *v, int n);
+void gauss_elim_spec(arma::vec a, arma::vec b, arma::vec c, arma::vec g, arma::vec *v, int n);
+
 void writetofile_exact(arma::vec x, arma::vec u, int n);
 void writetofile_approx(arma::vec x, arma::vec v, int n);
 void writetofile(arma::vec x, arma::vec u, arma::vec v, int n);
