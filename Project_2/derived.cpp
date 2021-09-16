4d98d1a8d79d51dcddf8bcea661e6616e65df972
kariannestrand <kariannestrand@outlook.com>
2021-09-16 12:26:53 +0200

    fully super class and derived class example completed

--- Project_2/derived.cpp
+++ Project_2/derived.cpp
@@ -1,11 +1,11 @@
 #include "derived.hpp"
 
-Quadratic::Quadratic(double c0, double c1, double c2) : Line(c0, c1){ //Here we reuse the constructor of the Line class. Note the syntax!
+DerivedClass::DerivedClass(double c0, double c1, double c2) : Line(c0, c1){ //Here we reuse the constructor of the Line class. Note the syntax!
     c2_ = c2; //Assign the higher order coefficient. The others are assigned in Line(c0,c1).
 }
 
-double Quadratic::compute_val(double x){
+double DerivedClass::compute_val(double x){
     //Computes the quadratic polynomial value y at x.
     //Reuses Line::compute_val(x) to compute the contribution from the straight line and tacks on the quadratic explicitly.
-    return Line::compute_val(x) + c2_*x*x;
+    return MyClass::compute_val(x) + c2_*x*x;
 }
