0999be6698936c6f2d42091c832fd293b5771d63
kariannestrand <kariannestrand@outlook.com>
2021-09-17 13:01:58 +0200

    did problem 4, but still pretty messy code

--- Project_2/main.cpp
+++ Project_2/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <iomanip>
 #include <chrono>
+#include <assert.h>
 
 using namespace arma;
 using namespace std;
@@ -12,14 +13,14 @@ using namespace std;
 mat num(double N, double a, double d);
 mat anal(double N);
 vec eigen_values(double N, double a, double d);
-double max_offdiag_symmetric(const arma::mat& B, int& k, int& l);
+double max_offdiag_symmetric(const mat& B, int& k, int& l);
 
 int main() {
-    double n = 7;
-    double N = (n-1);
-    double h = 1/n;
-    double a = -1/(h*h);
-    double d = 2/(h*h);
+    int n = 7;
+    int N = (n-1);
+    double h = 1./n;
+    double a = -1./(h*h);
+    double d = 2./(h*h);
 
     mat A = num(N, a, d);
     vec eigval;
@@ -29,6 +30,7 @@ int main() {
     mat V = normalise(anal(N), 2, 0);
     vec Lambda = eigen_values(N, a, d);
 
+    /*
     eigvec.print();
     cout << " " << endl;
     eigval.print();
@@ -36,6 +38,22 @@ int main() {
     V.print();
     cout << " " << endl;
     Lambda.print();
+    */
+
+    int s = 4;
+    mat B = mat(s, s).fill(0.);
+    B.eye();
+    B(3, 0) = 0.5;
+    B(0, 3) = 0.5;
+    B(1, 2) = -0.7;
+    B(2, 1) = -0.7;
+
+    int k;
+    int l;
+
+    double maxval = max_offdiag_symmetric(B, k, l);
+    cout << k <<  " " << l << endl;
+    cout << maxval << endl;
 
 
 
@@ -60,36 +78,21 @@ int main() {
 }
 
 
-// A function that finds the max off-diag element of a symmetric matrix A.
-// - The matrix indices of the max element are returned by writing to the
-//   int references k and l (row and column, respectively)
-// - The value of the max element A(k,l) is returned as the function
-//   return value
-double max_offdiag_symmetric(const arma::mat& B, int& k, int& l){
-    mat B = mat(N, N).fill(0.);
-    for i in range
-  // Get size of the matrix A. Use e.g. A.n_rows, see the Armadillo documentation
+double max_offdiag_symmetric(const mat &B, int &k, int &l){
+    int n = B.n_rows;                       // declears length of matrix
+    assert(B.is_square());                  // assert is an included function that makes sure the argument is true. We make sure the matrix is a nxn-matrix (square matrix)
 
-  // Possible consistency checks:
-  // Check that A is square and larger than 1x1. Here you can for instance use A.is_square(),
-  // see the Armadillo documentation.
-    cout << B.is_square() << endl;
-  //
-  // The standard function 'assert' from <assert.h> can be useful for quick checks like this
-  // during the code development phase. Use it like this: assert(some condition),
-  // e.g assert(a==b). If the condition evaluates to false, the program is killed with
-  // an assertion error. More info: https://www.cplusplus.com/reference/cassert/assert/
-
-  // Initialize references k and l to the first off-diagonal element of A
-
-  // Initialize a double variable 'maxval' to A(k,l). We'll use this variable
-  // to keep track of the largest off-diag element.
-
-  // Loop through all elements in the upper triangle of A (not including the diagonal)
-  // When encountering a matrix element with larger absolute value than the current value of maxval,
-  // update k, l and max accordingly.
-
-  return maxval; // Return maxval 
+    double maxval = 0.;                     // declears maxval
+    for (int j = 0; j < n; j++){            // the for loop iterates in the lower triangle of the matrix where the row index always is always bigger than the column index
+        for (int i = j+1; i < n; i++){
+            if (abs(B(i, j)) > maxval){     // if the next element is bigger than the previous, save it as maxval
+                maxval = abs(B(i, j));
+                k = i;                      // indices of row for maxval
+                l = j;                      // indices of column for maxval
+            }
+        }
+    }
+    return maxval;
 }
 
 mat num(double N, double a, double d){
