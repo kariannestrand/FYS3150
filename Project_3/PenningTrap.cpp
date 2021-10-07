5f40e60b6c09898b1ef908a3e200293721b2e8d1
kariannestrand <kariannestrand@outlook.com>
2021-10-07 14:05:43 +0200

    fixed merge things

--- Project_3/PenningTrap.cpp
+++ Project_3/PenningTrap.cpp
@@ -25,20 +25,14 @@ vec PenningTrap::external_B_field(vec r){
     return B;
 }
 
-<<<<<<< HEAD
-vec PenningTrap::external_E_field(vec r){
+
+vec PenningTrap::external_E_field(int i){
     vec F = vec(3).fill(0.);
     // position of E-field
     F(0) = -1.;
-=======
-vec PenningTrap::external_E_field(int i){
-    vec F = vec(3).fill(0.); 
-    // position of E-field       
-    F(0) = -1.;                     
->>>>>>> 0d928b2aeac6f7a1ce5159934a9ba928810924da
     F(1) = -1.;
     F(2) = 2.;
-    
+
     vec E = (-V0_/(d_*d_)*F) % particles_[i].r_;      // E-field
 
     return E;
@@ -46,40 +40,26 @@ vec PenningTrap::external_E_field(int i){
 
 vec PenningTrap::force_particle(int i, int j){
     vec F = vec(3).fill(0.);
-<<<<<<< HEAD
-    for (int i = 0; i < n_; i++){
-        for (int j = 0; j < n_; j++){
-            if (i !=j){
-            F = ke_ * (particles_[i].q_ * particles_[j].q_) / ((particles_[i].r_ % particles_[i].r_) + (particles_[j].r_ % particles_[j].r_)) % abs((particles_[i].r_ - particles_[j].r_));
-            }
-        }
-    }
 
-=======
     vec r = particles_[i].r_ - particles_[j].r_;
-    F = ke_ * (particles_[i].q_ * particles_[j].q_) / (abs(r)%abs(r)%abs(r)) % r; 
->>>>>>> 0d928b2aeac6f7a1ce5159934a9ba928810924da
+    F = ke_ * (particles_[i].q_ * particles_[j].q_) / (abs(r)%abs(r)%abs(r)) % r;
+
     return F;
 }
 
 vec PenningTrap::total_force_external(vec E, vec B, int i){
     vec F = vec(3).fill(0.);
-<<<<<<< HEAD
     for (int i = 0; i < n_; i++){
         F = particles_[i].q_ * E + cross(particles_[i].q_ * particles_[i].v_, B);
     }
-=======
+
     F = particles_[i].q_ * E + cross(particles_[i].q_ * particles_[i].v_, B);
 
->>>>>>> 0d928b2aeac6f7a1ce5159934a9ba928810924da
     return F;
 }
 
-
-
-<<<<<<< HEAD
 }
-=======
+
 vec PenningTrap::total_force_particles(vec F_particles){
     vec F;
     for (int i = 0; i < n_; i++){
@@ -91,8 +71,7 @@ vec PenningTrap::total_force_particles(vec F_particles){
     }
 
     return F;
-}  
->>>>>>> 0d928b2aeac6f7a1ce5159934a9ba928810924da
+}
 
 /*
 vec PenningTrap::total_force(int i){
