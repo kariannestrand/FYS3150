2943be4236cc2def48ab7a5e602f8a8c83f1d67d
mariaoftedahl <mloftedahl@gmail.com>
2021-10-14 11:53:56 +0200

    made python file for plotting

---
+++ Project_3/main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+n_particles = 2 # number of particles
+
+for i in range(n_particles):
+    filename = "Euler_r.txt"
+    Rx = []
+    Ry = []
+    Rz = []
+    with open(filename) as f:
+        for line in f.read().split("\n")[0::4]:
+            cols = line.split()
+            if len(cols) > 1:
+                Rx.append(cols[i])
+
+    Rx = np.array(Rx)
+
+    with open(filename) as f:
+        for line in f.read().split("\n")[1::4]:
+            cols = line.split()
+            if len(cols) > 1:
+                Ry.append(cols[i])
+
+    Ry = np.array(Ry)
+
+    with open(filename) as f:
+        for line in f.read().split("\n")[2::4]:
+            cols = line.split()
+            if len(cols) > 1:
+                Rz.append(cols[i])
+
+    Rz = np.array(Rz)
+    t = np.linspace(0,100,len(Rz))
+
+
