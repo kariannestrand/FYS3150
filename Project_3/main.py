f694b096c0c3ec70adc290a6fe8660b85b9f0531
kariannestrand <kariannestrand@outlook.com>
2021-10-22 10:42:16 +0200

    cleaned up, deleted all things txt

--- Project_3/main.py
+++ Project_3/main.py
@@ -1,35 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
+import pyarma as pa
 
 
 n = 1                            # number of particles
-
-# buttons
 interaction = False
-read_velocity = True             # must always be True
-read_position = True             # must always be True
 
 z_t = False
 x_y = False
-
 phase_space = False
 trajectory = False
-relative_error = True
-error_convergence_rate = True
-
-
+relative_error = False
+error_convergence_rate = False
 
 v_x = phase_space
 v_y = phase_space
 v_z = phase_space
-
-relative_error_RK4 = relative_error
-relative_error_Euler = relative_error
-
-error_convergence_rate_RK4 = error_convergence_rate
-error_convergence_rate_Euler = error_convergence_rate
-
 
 
 # constants
@@ -47,20 +34,41 @@ omega_p = 0.5*(omega_0 + np.sqrt(omega_0**2 - 2*omega_z**2));
 omega_m = 0.5*(omega_0 - np.sqrt(omega_0**2 - 2*omega_z**2));
 
 
+def V(filename_v):
+    V = pa.mat()
+    V.load(filename_v)
+    vel = np.transpose(V)
+
+    x = vel[:, 0]
+    y = vel[:, 1]
+    z = vel[:, 2]
+
+    return x, y, z
+
+
+def R(filename_r):
+    R = pa.mat()
+    R.load(filename_r)
+    pos = np.transpose(R)
+
+    x = pos[:, 0]
+    y = pos[:, 1]
+    z = pos[:, 2]
+
+    return x, y, z
+
+
 def r_analytical(filename_r, filename_v, h):
+    Rx = R(filename_r)[0]
+    Rz = R(filename_r)[2]
 
-    pos = np.loadtxt(filename_r, skiprows = 1)
-    Rx1 = pos[:, 0]
-    Rz1 = pos[:, 2]
+    Vy = V(filename_v)[1]
 
-    vel = np.loadtxt(filename_v, skiprows = 1)
-    Vy1 = pos[:, 1]
+    N = len(Rx)
 
-    N = len(Rx1)
-
-    x_0 = Rx1[0]
-    v_0 = Vy1[0]
-    z_0 = Rz1[0]
+    x_0 = Rx[0]
+    v_0 = Vy[0]
+    z_0 = Rz[0]
 
 
     Ap = (v_0 + omega_m*x_0)/(omega_m - omega_p)
@@ -85,22 +93,15 @@ def r_analytical(filename_r, filename_v, h):
 
 
 def r_numerical(filename_r):
-    pos = np.loadtxt(filename_r, skiprows = 1)
-    Rx1 = pos[:, 0]
-    Ry1 = pos[:, 1]
-    Rz1 = pos[:, 2]
+    Rx = R(filename_r)[0]
+    Ry = R(filename_r)[1]
+    Rz = R(filename_r)[2]
 
-    N = len(Rx1)
+    N = len(Rx)
 
     r_num = np.empty(N)
     for i in range(N):
-        r_num[i] = np.sqrt(Rx1[i]**2 + Ry1[i]**2 + Rz1[i]**2)
-
-    """
-    t = np.linspace(0, 100, N)
-    plt.plot(t, r_num)
-    plt.show()
-    """
+        r_num[i] = np.sqrt(Rx[i]**2 + Ry[i]**2 + Rz[i]**2)
 
     return r_num
 
@@ -113,6 +114,21 @@ def relative_error(filename_r, filename_v, h):
 
     return rel_err
 
+def error_convergence_rate(filename_r_list, filename_v_list, h_list):
+    N = len(h_list)
+    delta_max = np.empty(N)
+
+    for i in range(N):
+        r_exact = r_analytical(filename_r_list[i], filename_v_list[i], h_list[i])
+        r_num = r_numerical(filename_r_list[i])
+
+        delta_max[i] = np.max(np.abs(r_exact - r_num))
+
+    r_err = 0
+    for i in range(1, N):
+        r_err += 0.25*np.log10(delta_max[i]/delta_max[i-1])/np.log10(h_list[i]/h_list[i-1])
+
+    return r_err
 
 def time(filename_r):
     N = len(r_numerical(filename_r))
@@ -120,52 +136,15 @@ def time(filename_r):
     return t
 
 
-if read_velocity:
-    filename = "RK4_v_1_0001dt.txt"
-    vel = np.loadtxt(filename, skiprows = 1)
-    Vx1 = vel[:, 0]
-    Vy1 = vel[:, 1]
-    Vz1 = vel[:, 2]
-
-
-    if n == 2:
-        filename = "RK4_v_2_0001dt.txt"
-        vel = np.loadtxt(filename, skiprows = 1)
-        Vx2 = vel[:, 0]
-        Vy2 = vel[:, 1]
-        Vz2 = vel[:, 2]
-
-
-if read_position:
-    filename = "RK4_r_1_0001dt.txt"
-    pos = np.loadtxt(filename, skiprows = 1)
-    Rx1 = pos[:, 0]
-    Ry1 = pos[:, 1]
-    Rz1 = pos[:, 2]
-
-
-    if n == 2:
-        filename = "RK4_r_2_0001dt.txt"
-        pos = np.loadtxt(filename, skiprows = 1)
-        Rx2 = pos[:, 0]
-        Ry2 = pos[:, 1]
-        Rz2 = pos[:, 2]
-
+filename_r = "r_0_0001.bin"
+filename_v = "v_0_0001.bin"
 
 if z_t:
-    t = np.linspace(0, 100, len(Rz1))
+    #fig = plt.figure()
+    Rz = R(filename_r)[2]
+    t = time(filename_r)
 
-    z_min = np.min(Rz1)
-    index = np.where(Rz1 == Rz1.min())
-    f = 1/(np.max(t[index]) - np.min(t[index]))
-
-    print(omega_z)    # frequency of particle about the z-axis
-    print(f)          # trying to calculate frequency from plot
-
-
-    fig = plt.figure()
-    plt.plot(t, Rz1)
-
+    plt.plot(t, Rz)
 
     if n == 1:
         plt.title("Movement of one particle in the z-direction", size = 12)
@@ -193,10 +172,11 @@ if z_t:
 
     plt.show()
 
-
 if x_y:
-    fig = plt.figure()
-    plt.plot(Rx1, Ry1)
+    #fig = plt.figure()
+    Rx = R(filename_r)[0]
+    Ry = R(filename_r)[1]
+    plt.plot(Rx, Ry)
 
     if n == 1:
         plt.title("Movement of one particle in the xy-plane", size = 10)
@@ -225,13 +205,14 @@ if x_y:
 
     plt.show()
 
-
 if v_x:
-    fig = plt.figure()
-    plt.plot(Rx1, Vx1)
+    #fig = plt.figure()
+    Rx = R(filename_r)[0]
+    Vx = V(filename_v)[0]
+    plt.plot(Rx, Vx)
 
     if n == 1:
-        plt.title("IDK YET", size = 10)
+        plt.title("Velocity as function of position in the x-direction", size = 10)
 
     if n == 2:
         plt.plot(Rx2, Vx2)
@@ -256,13 +237,14 @@ if v_x:
 
     plt.show()
 
-
 if v_y:
-    fig = plt.figure()
-    plt.plot(Ry1, Vy1)
+    #fig = plt.figure()
+    Ry = R(filename_r)[1]
+    Vy = V(filename_v)[1]
+    plt.plot(Ry, Vy)
 
     if n == 1:
-        plt.title("IDK YET", size = 10)
+        plt.title("Velocity as function of position in the y-direction", size = 10)
 
     if n == 2:
         plt.plot(Ry2, Vy2)
@@ -287,13 +269,14 @@ if v_y:
 
     plt.show()
 
-
 if v_z:
-    fig = plt.figure()
-    plt.plot(Rz1, Vz1)
+    #fig = plt.figure()
+    Rz = R(filename_r)[2]
+    Vz = V(filename_v)[2]
+    plt.plot(Rz, Vz)
 
     if n == 1:
-        plt.title("IDK YET", size = 10)
+        plt.title("Velocity as function of position in the z-direction", size = 10)
 
     if n == 2:
         plt.plot(Rz2, Vz2)
@@ -318,13 +301,17 @@ if v_z:
 
     plt.show()
 
-
 if trajectory:
-    fig = plt.figure()
+    #fig = plt.figure()
+
+    Rx = R(filename_r)[0]
+    Ry = R(filename_r)[1]
+    Rz = R(filename_r)[2]
+
     ax = plt.axes(projection="3d")
     plt.tight_layout()
-    ax.plot3D(Rx1, Ry1, Rz1, 'blue', label='Trajectory of particle 1')
-    plt.plot(Rx1[0], Ry1[0], Rz1[0], "ro")
+    ax.plot3D(Rx, Ry, Rz, 'blue', label='Trajectory of particle 1')
+    plt.plot(Rx[0], Ry[0], Rz[0], "ro")
 
     if n == 1:
         plt.title("Trajectory of one particle", size = 12)
@@ -357,91 +344,29 @@ if trajectory:
     plt.show()
 
 
-if relative_error_RK4:
-    filename_r_RK4 = ["RK4_r_1_1dt.txt", "RK4_r_1_01dt.txt", "RK4_r_1_001dt.txt", "RK4_r_1_0001dt.txt", "RK4_r_1_00001dt.txt"]
-    filename_v_RK4 = ["RK4_v_1_1dt.txt", "RK4_v_1_01dt.txt", "RK4_v_1_001dt.txt", "RK4_v_1_0001dt.txt", "RK4_v_1_00001dt.txt"]
+if relative_error:
+    filename_r_list = ["r_0_0001.bin", "r_0_0001.bin", "r_0_0001.bin", "r_0_0001.bin", "r_0_0001.bin"]
+    filename_v_list = ["v_0_0001.bin", "v_0_0001.bin", "v_0_0001.bin", "v_0_0001.bin", "v_0_0001.bin"]
+    h_list = [1., 0.1, 0.01, 0.001, 0.0001]
 
-    h = [1., 0.1, 0.01, 0.001, 0.0001]
-    for i in range(len(filename_r_RK4)):
-        plt.plot(time(filename_r_RK4[i]), relative_error(filename_r_RK4[i], filename_v_RK4[i], h[i]), label = "h = " + str(h[i]))
+
+    N = len(h_list)
+    for i in range(N):
+        t = time(filename_r_list[i])
+        rel_err = relative_error(filename_r_list[i], filename_v_list[i], h_list[i])
+        plt.plot(t, rel_err, label = "h = " + str(h_list[i]))
 
     plt.title("Relative Error with RK4", size = 12)
-
     plt.xlabel("t/[$\mu$s]", size = 12)
     plt.ylabel("$|(r_{exact} - r_{numerical})/r_{numerical}|$", size = 12)
-
-    #plt.ticklabel_format(axis="both", style="sci", scilimits=(0,0))
     plt.legend()
-
     plt.savefig('pdf/rel_err_RK4.pdf')
-
     plt.show()
 
+if error_convergence_rate:
+    filename_r_list = ["r_0_0001.bin", "r_0_0001.bin", "r_0_0001.bin", "r_0_0001.bin", "r_0_0001.bin"]
+    filename_v_list = ["v_0_0001.bin", "v_0_0001.bin", "v_0_0001.bin", "v_0_0001.bin", "v_0_0001.bin"]
+    h_list = [1., 0.1, 0.01, 0.001, 0.0001]
 
-if relative_error_Euler:
-    filename_r_Euler = ["Euler_r_1_1dt.txt", "Euler_r_1_01dt.txt", "Euler_r_1_001dt.txt", "Euler_r_1_0001dt.txt", "Euler_r_1_00001dt.txt"]
-    filename_v_Euler = ["Euler_v_1_1dt.txt", "Euler_v_1_01dt.txt", "Euler_v_1_001dt.txt", "Euler_v_1_0001dt.txt", "Euler_v_1_00001dt.txt"]
-
-    h = [1., 0.1, 0.01, 0.001, 0.0001]
-    for i in range(len(filename_r_Euler)):
-        plt.plot(time(filename_r_Euler[i]), relative_error(filename_r_Euler[i], filename_v_Euler[i], h[i]), label = "h = " + str(h[i]))
-
-    plt.title("Relative Error with Forward Euler", size = 12)
-
-    plt.xlabel("t/[$\mu$s]", size = 12)
-    plt.ylabel("$|(r_{exact} - r_{numerical})/r_{numerical}|$", size = 12)
-
-    #plt.ticklabel_format(axis="both", style="sci", scilimits=(0,0))
-    plt.legend()
-
-    plt.savefig('pdf/rel_err_Euler.pdf')
-
-    plt.show()
-
-
-if error_convergence_rate_RK4:
-    filename_r_RK4 = ["RK4_r_1_1dt.txt", "RK4_r_1_01dt.txt", "RK4_r_1_001dt.txt", "RK4_r_1_0001dt.txt", "RK4_r_1_00001dt.txt"]
-    filename_v_RK4 = ["RK4_v_1_1dt.txt", "RK4_v_1_01dt.txt", "RK4_v_1_001dt.txt", "RK4_v_1_0001dt.txt", "RK4_v_1_00001dt.txt"]
-
-    h = [1., 0.1, 0.01, 0.001, 0.0001]
-
-    Delta_max = np.empty(len(h))
-    for i in range(len(h)):
-        r_exact = r_analytical(filename_r_RK4[i], filename_v_RK4[i], h[i])
-        r_num = r_numerical(filename_r_RK4[i])
-
-        Delta_max[i] = np.max(np.abs(r_exact - r_num))
-
-    r_err = 0
-    for i in range(1, len(h)):
-        r_err += 0.25*np.log10(Delta_max[i]/Delta_max[i-1])/np.log10(h[i]/h[i-1])
-
+    r_err = error_convergence_rate(filename_r_list, filename_v_list, h_list)
     print("Error convergence rate with RK4: r_err = {}".format(r_err))
-
-
-if error_convergence_rate_Euler:
-    filename_r_Euler = ["Euler_r_1_1dt.txt", "Euler_r_1_01dt.txt", "Euler_r_1_001dt.txt", "Euler_r_1_0001dt.txt", "Euler_r_1_00001dt.txt"]
-    filename_v_Euler = ["Euler_v_1_1dt.txt", "Euler_v_1_01dt.txt", "Euler_v_1_001dt.txt", "Euler_v_1_0001dt.txt", "Euler_v_1_00001dt.txt"]
-
-    h = [1., 0.1, 0.01, 0.001, 0.0001]
-
-    Delta_max = np.empty(len(h))
-    for i in range(len(h)):
-        r_exact = r_analytical(filename_r_Euler[i], filename_v_Euler[i], h[i])
-        r_num = r_numerical(filename_r_Euler[i])
-
-        Delta_max[i] = np.max(np.abs(r_exact - r_num))
-
-    r_err = 0
-    for i in range(1, len(h)):
-        r_err += 0.25*np.log10(Delta_max[i]/Delta_max[i-1])/np.log10(h[i]/h[i-1])
-
-    print("Error convergence rate with Euler: r_err = {}".format(r_err))
-
-
-#filename_r = "RK4_r_1_0001dt.txt"
-#filename_v = "RK4_v_1_0001dt.txt"
-#h = 0.001
-
-#r_analytical(filename_r, filename_v, h)
-#r_numerical(filename_r)
