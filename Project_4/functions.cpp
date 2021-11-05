2935caeded53064f511ce8ef5268ea429c92b7ab
kariannestrand <kariannestrand@outlook.com>
2021-11-05 15:56:40 +0100

    made periodic boundary conditions

---
+++ Project_4/functions.cpp
@@ -0,0 +1,32 @@
+#include "functions.hpp"
+
+using namespace arma;
+using namespace std;
+
+// function to initialize spin configuration, energy and magnetization
+void Initialize(int L, mat *S, double *E, double *M){
+    for(int i = 0; i < L; i++) {
+        for (int j = 0; j < L; j++){
+
+            if ((*S)(i, j) >= 0.5){
+                (*S)(i, j) = 1.0;          // spin configuration, spin up
+            }
+            else{
+                (*S)(i, j) = - 1.0;        // spin configuration, spin down
+            }
+
+            (*E) -= (*S)(i, j) * ((*S)(PBC(i, L, -1), j) + (*S)(i, PBC(j, L, -1)));
+            (*M) += (*S)(i, j);
+
+        }
+    }
+}
+
+
+// inline function for periodic boundary conditions
+inline int PBC(int i, int limit, int add){
+    // metropolis sampling is used, periodic boundary conditions
+    // i: base index, limit: highest legal index (L), add: number added to i
+    // a % b gives the remainder of a/b
+    return (i + limit + add) % (limit);
+}
---
+++ Project_4/functions.hpp
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_
+#define FUNCTIONS_
+
+#include <armadillo>
+#include <iostream>
+
+// Declearing functions:
+void Initialize(int L, arma::mat *S, double *E, double *M);
+inline int PBC(int i, int limit, int add);
+
+#endif
