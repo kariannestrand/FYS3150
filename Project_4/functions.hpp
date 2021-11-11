57819a00148e873c16ee2374f247bdf784df7226
kariannestrand <kariannestrand@outlook.com>
2021-11-11 14:00:47 +0100

    cleaned folders

--- Project_4/functions.hpp
+++ Project_4/functions.hpp
@@ -5,7 +5,10 @@
 #include <iostream>
 
 // Declearing functions:
-void Initialize(int L, arma::mat *S, double *E, double *M);
+arma::mat spin_matrix(int L);
+void initialize(int L, arma::mat &S, double &E, double &M, int N);
+int delta_E(arma::mat &S, int L, int i, int j);
+void metropolis(arma::mat &S, int L, double T, double &E, double &M, int N_cycles, int N);
 inline int PBC(int i, int limit, int add);
 
 #endif
