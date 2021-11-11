c63be4b3da4fbf2e7bdc069f1ab3931f7b433c47
mariaoftedahl <mloftedahl@gmail.com>
2021-11-11 13:30:02 +0100

    parallelizing

--- Project_4/test/main.cpp
+++ Project_4/test/main.cpp
@@ -21,25 +21,27 @@ int main(int argc, char const *argv[]){
     int L = 20;                         // lattice length
     double N = L*L;                     // number of spins
     double T = 2.1;
-    double NT = 1000;
+    int NT = 1000;
     vec T_vec = linspace(2.1, 2.4, NT);
     int N_cycles = 1000;
     
 
-    mat S = spin_matrix(L);
+    //mat S = spin_matrix(L);
 
     double E = 0.;                      // initialize energy
     double M = 0.;                      // initialize magnetization
 
-    initialize(L, S, E, M, N);
+    //initialize(L, S, E, M, N);
     //metropolis(S, L, T, E, M, N_cycles, N);
     
 
     bool timing = true;
     if (timing){
         auto t0 = std::chrono::high_resolution_clock::now();
-        #pragma omp parallel // Start parallel region
+        #pragma omp parallel private(E, M) // Start parallel region
         {
+            mat S = spin_matrix(L);
+            initialize(L, S, E, M, N);
             #pragma omp for
             for(int i = 0; i < NT; i++){
                 metropolis(S, L, T_vec(i), E, M, N_cycles, N);
@@ -48,7 +50,7 @@ int main(int argc, char const *argv[]){
         auto t = std::chrono::high_resolution_clock::now();
         double duration_seconds_wo = std::chrono::duration<double>(t - t0).count();
 
-        cout << "time used without OpenMP = " << duration_seconds_wo << " seconds\n";
+        cout << "time used with OpenMP = " << duration_seconds_wo << " seconds\n";
     }
     return 0;
 }
