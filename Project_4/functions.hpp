7e40ba2296fd2ea23e3eca6ea9399a61721b2ba7
kariannestrand <kariannestrand@outlook.com>
2021-11-18 14:34:58 +0100

    commented, and made true false things

--- Project_4/functions.hpp
+++ Project_4/functions.hpp
@@ -7,8 +7,8 @@
 // Declearing functions:
 inline int PBC(int i, int limit, int add);
 arma::mat spin_matrix(int L);
-void initialize(int L, arma::mat &S, double &E, double &M, int N);
+void initialize(int L, arma::mat &S, double &E, double &M, int N, bool random);
 int delta_E(arma::mat &S, int L, int i, int j);
-void metropolis(arma::mat &S, int L, double T, double &E, double &M, int N_cycles, int N);
+void metropolis(arma::mat &S, int L, double T, double &E, double &M, int N_cycles, int N, bool write);
 
 #endif
