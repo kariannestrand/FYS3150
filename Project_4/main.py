b0f13a717a0997e74aa94ca2e04909fecad44564
kariannestrand <kariannestrand@outlook.com>
2021-11-22 17:17:30 +0100

    i have done a lot a dont know whats gonna happen

--- Project_4/main.py
+++ Project_4/main.py
@@ -5,6 +5,7 @@ from scipy.stats import linregress
 burn_in_time = False
 histogram = False
 phase_transition = False
+critical_temperature = False
 
 # investigating the burnin time
 if burn_in_time:
@@ -17,127 +18,117 @@ if burn_in_time:
     filename7 = "m_exp_24_ordered.bin"
     filename8 = "m_exp_24_unordered.bin"
 
-    loadtxt1 = np.loadtxt(filename1, skiprows = 0)
-    loadtxt2 = np.loadtxt(filename2, skiprows = 0)
-    loadtxt3 = np.loadtxt(filename3, skiprows = 0)
-    loadtxt4 = np.loadtxt(filename4, skiprows = 0)
-    loadtxt5 = np.loadtxt(filename5, skiprows = 0)
-    loadtxt6 = np.loadtxt(filename6, skiprows = 0)
-    loadtxt7 = np.loadtxt(filename7, skiprows = 0)
-    loadtxt8 = np.loadtxt(filename8, skiprows = 0)
+    eps_exp24_ord = np.loadtxt(filename1, skiprows = 0)
+    eps_exp1_ord = np.loadtxt(filename2, skiprows = 0)
+    eps_exp24_un = np.loadtxt(filename3, skiprows = 0)
+    eps_exp1_un = np.loadtxt(filename4, skiprows = 0)
+    m_exp1_ord = np.loadtxt(filename5, skiprows = 0)
+    m_exp1_un = np.loadtxt(filename6, skiprows = 0)
+    m_exp24_ord = np.loadtxt(filename7, skiprows = 0)
+    m_exp24_un = np.loadtxt(filename8, skiprows = 0)
 
-    eps_exp24_ord = loadtxt1
-    eps_exp1_ord = loadtxt2
-    eps_exp24_un = loadtxt3
-    eps_exp1_un = loadtxt4
-    m_exp1_ord = loadtxt5
-    m_exp1_un = loadtxt6
-    m_exp24_ord = loadtxt7
-    m_exp24_un = loadtxt8
-    n = 100000  #number of cycles
-    T = np.linspace(0,1000001,n)/1000
+    n = len(eps_exp24_ord)  #number of cycles
+    T = np.linspace(0, n, n)
 
-    plt.plot(T, eps_exp24_ord)
-    plt.plot(T, eps_exp24_un)
-    plt.rc('xtick', labelsize=10)
-    plt.rc('ytick', labelsize=10)
-    plt.ylabel('$<\epsilon>$')
-    plt.xlabel('t/[$10^3$ cycles]')
-    plt.savefig('eps_burnin.pdf')
-    plt.show()
-
-    plt.plot(T, eps_exp1_ord)
-    plt.plot(T, eps_exp1_un)
-    plt.rc('xtick', labelsize=20)
-    plt.rc('ytick', labelsize=20)
-    plt.show()
-    plt.plot(T, m_exp1_ord)
-    plt.plot(T, m_exp1_un)
-    plt.rc('xtick', labelsize=20)
-    plt.rc('ytick', labelsize=20)
-    plt.show()
-    plt.plot(T, m_exp24_ord)
-    plt.plot(T, m_exp24_un)
-    plt.rc('xtick', labelsize=20)
-    plt.rc('ytick', labelsize=20)
+    plt.plot(T, eps_exp24_ord, label = "T = 2.4 J/$k_B$ - Ordered")
+    plt.plot(T, eps_exp24_un, label = "T = 2.4 J/$k_B$ - Unordered")
+    plt.plot(T, eps_exp1_ord, label = "T = 1.0 J/$k_B$ - Ordered")
+    plt.plot(T, eps_exp1_un, label = "T = 1.0 J/$k_B$ - Unordered")
+    plt.ylabel("Expected energy per spin/[J]", size = 20)
+    plt.xlabel("Number of Monte Carlo cycles", size = 20)
+    plt.xticks(size = 17); plt.xscale("log")
+    plt.yticks(size = 17)
+    plt.legend(fontsize = 14)
     plt.show()
 
 
-
+    plt.plot(T, m_exp24_ord, label = "T = 2.4 J/$k_B$ - Ordered")
+    plt.plot(T, m_exp24_un, label = "T = 2.4 J/$k_B$ - Unordered")
+    plt.plot(T, m_exp1_ord, label = "T = 1.0 J/$k_B$ - Ordered")
+    plt.plot(T, m_exp1_un, label = "T = 1.0 J/$k_B$ - Unordered")
+    plt.ylabel("Expected absolute magnetization per spin", size = 17)
+    plt.xlabel("Number of Monte Carlo cycles", size = 20)
+    plt.xticks(size = 17); plt.xscale("log")
+    plt.yticks(size = 17)
+    plt.legend(fontsize = 14)
+    plt.show()
 
 # making histograms of samples of the expectation value of the energy
 if histogram:
-    filename1 = 'eps_histo_1.bin'
-    filename2 = 'eps_histo_24.bin'
-    loadtxt1 = np.loadtxt(filename1, skiprows = 0)
-    loadtxt2 = np.loadtxt(filename2, skiprows = 0)
-    eps_1 = loadtxt1
-    eps_2 = loadtxt2
+    filename1 = "eps_histo_1_unord.bin"
+    filename24 = "eps_histo_24_unord.bin"
 
-    n, bins, patches = plt.hist(eps_1, 180, density=True)
-    plt.xlim(-2.01,-1.8)
-    plt.ylabel('Probability', fontsize=15)
-    plt.xlabel('$\epsilon$', fontsize=15)
-    plt.xticks(fontsize=10)
-    plt.yticks(fontsize=10)
-    plt.savefig('histo_1.pdf')
-    plt.show()
-    n, bins, patches = plt.hist(eps_2, 120, density=True)
-    plt.ylabel('Probability', fontsize=10)
-    plt.xlabel('$\epsilon$', fontsize=10)
-    plt.xticks(fontsize=10)
-    plt.yticks(fontsize=10)
-    plt.savefig('histo_2.pdf')
+    eps_1 = np.loadtxt(filename1, skiprows = 0)
+    eps_24 = np.loadtxt(filename24, skiprows = 0)
+
+    n, bins, patches = plt.hist(eps_1, 120, density = True)
+    plt.title("Probability of energy per spin with T = 1.0 J/$k_B$", size = 22)
+    plt.ylabel('Normalized frequency', fontsize = 20)
+    plt.xlabel('Energy per spin/[J]', fontsize = 20)
+    plt.xticks(fontsize = 17)
+    plt.yticks(fontsize = 17)
+    plt.legend()
     plt.show()
 
-# for testing the files
-filename = 'chi_100L_noburnin.bin'
-loadtxt = np.loadtxt(filename, skiprows = 0)
-chi = loadtxt[:, 0]
-T = loadtxt[:, 1]
-T, chi = zip(*sorted(zip(T, chi)))
-T_max = T[np.argmax(chi)]
-#print(T_max)
-plt.plot(T, chi, 'o')
-plt.savefig('test.pdf')
-plt.show()
-
+    n, bins, patches = plt.hist(eps_24, 120, density = True)
+    plt.title("Probability of energy per spin with T = 2.4 J/$k_B$", size = 22)
+    plt.ylabel('Normalized frequency', fontsize = 20)
+    plt.xlabel('Energy per spin/[J]', fontsize = 20)
+    plt.xticks(fontsize = 17)
+    plt.yticks(fontsize = 17)
+    plt.legend()
+    plt.show()
 
 # investigating phace transitions
 if phase_transition:
+    L = ["40", "60", "80", "100"]
+
     filename_list_eps = ["eps_exp_40L.bin", "eps_exp_60L.bin", "eps_exp_80L.bin", "eps_exp_100L.bin"]
     for i in range(len(filename_list_eps)):
         filename = filename_list_eps[i]
         loadtxt = np.loadtxt(filename, skiprows = 0)
-        eps_exp = loadtxt[:, 0]
-        T = loadtxt[:, 1]
-        T, cV = zip(*sorted(zip(T, eps_exp)))
-        plt.plot(T, eps_exp, 'o')
-        plt.show()
+        eps_exp_unsorted = loadtxt[:, 0]
+        T_unsorted = loadtxt[:, 1]
+        T, e_exp = zip(*sorted(zip(T_unsorted, eps_exp_unsorted)))
+        plt.plot(T, e_exp, '.', label = "L = " + L[i])
+        plt.xlabel("Temperature/[J/$k_B$]", size = 20)
+        plt.ylabel("Expected energy per spin/[J]", size = 20)
+        plt.xticks(size = 17)
+        plt.yticks(size = 17)
+        plt.legend(fontsize = 14)
+    plt.show()
 
 
     filename_list_m = ["m_exp_40L.bin", "m_exp_60L.bin", "m_exp_80L.bin", "m_exp_100L.bin"]
     for i in range(len(filename_list_m)):
         filename = filename_list_m[i]
         loadtxt = np.loadtxt(filename, skiprows = 0)
-        m_exp = loadtxt[:, 0]
-        T = loadtxt[:, 1]
-        T, m_exp = zip(*sorted(zip(T, m_exp)))
-        plt.plot(T, m_exp, 'o')
-        plt.show()
+        m_exp_unsorted = loadtxt[:, 0]
+        T_unsorted = loadtxt[:, 1]
+        T, m_exp = zip(*sorted(zip(T_unsorted, m_exp_unsorted)))
+        plt.plot(T, m_exp, '.', label = "L = " + L[i])
+        plt.xlabel("Temperature/[J/$k_B$]", size = 20)
+        plt.ylabel("Expected absolute magnetization per spin", size = 20)
+        plt.xticks(size = 17)
+        plt.yticks(size = 17)
+        plt.legend(fontsize = 14)
+    plt.show()
 
 
-    filename_list_cv = ["cV_40L.bin", "cV_60L.bin", "cV_80L.bin", "cV_100L.bin"]
-    for i in range(len(filename_list_cv)):
-        filename = filename_list_cv[i]
+    filename_list_cV = ["cV_40L.bin", "cV_60L.bin", "cV_80L.bin", "cV_100L.bin"]
+    for i in range(len(filename_list_cV)):
+        filename = filename_list_cV[i]
         loadtxt = np.loadtxt(filename, skiprows = 0)
-        cV = loadtxt[:, 0]
-        T = loadtxt[:, 1]
-        T, cV = zip(*sorted(zip(T, cV)))
-        T_max = T[np.argmax(cV)]
-        print(T_max)
-        plt.plot(T, cV, 'o')
-        plt.show()
+        cV_unsorted = loadtxt[:, 0]
+        T_unsorted = loadtxt[:, 1]
+        T, cV = zip(*sorted(zip(T_unsorted, cV_unsorted)))
+        plt.plot(T, cV, '.', label = "L = " + L[i])
+        plt.xlabel("Temperature/[J/$k_B$]", size = 20)
+        plt.ylabel("Heat capacity per spin/[$k_B$]", size = 20)
+        plt.xticks(size = 17)
+        plt.yticks(size = 17)
+        plt.legend(fontsize = 14)
+    plt.show()
 
 
 
@@ -145,10 +136,57 @@ if phase_transition:
     for i in range(len(filename_list_chi)):
         filename = filename_list_chi[i]
         loadtxt = np.loadtxt(filename, skiprows = 0)
-        chi = loadtxt[:, 0]
-        T = loadtxt[:, 1]
-        T, chi = zip(*sorted(zip(T, chi)))
-        T_max = T[np.argmax(chi)]
-        print(T_max)
-        plt.plot(T, chi, 'o')
-        plt.show()
+        chi_unsorted = loadtxt[:, 0]
+        T_unsorted = loadtxt[:, 1]
+        T, chi = zip(*sorted(zip(T_unsorted, chi_unsorted)))
+        plt.plot(T, chi, '.', label = "L = " + L[i])
+        plt.xlabel("Temperature/[J/$k_B$]", size = 20)
+        plt.ylabel("Suceptibility per spin/[1/J]", size = 20)
+        plt.xticks(size = 17)
+        plt.yticks(size = 17)
+        plt.legend(fontsize = 14)
+    plt.show()
+
+# estimating critical temperature
+if critical_temperature:
+    T_max_list = []
+
+    L = ["40", "60", "80", "100"]
+    filename_list_chi = ["chi_40L.bin", "chi_60L.bin", "chi_80L.bin", "chi_100L.bin"]
+    for i in range(len(filename_list_chi)):
+        filename = filename_list_chi[i]
+        loadtxt = np.loadtxt(filename, skiprows = 0)
+        chi_unsorted = loadtxt[:, 0]
+        T_unsorted = loadtxt[:, 1]
+        T, chi = zip(*sorted(zip(T_unsorted, chi_unsorted)))
+        chi_max = np.amax(chi)
+        chi_max_index = np.argmax(chi)
+        T_max = T[chi_max_index]
+        T_max_list.append(T_max)
+        plt.plot(T, chi)
+        plt.plot(T_max, chi_max, "o", label = "$T_c$(" + L[i] + ") = " + str(T_max) + " J/$k_B$")
+        plt.xlabel("Temperature/[J/$k_B$]", size = 20)
+        plt.ylabel("Suceptibility per spin/[1/J]", size = 20)
+        plt.xticks(size = 17)
+        plt.yticks(size = 17)
+        plt.legend(fontsize = 17)
+    plt.show()
+
+    L_array = np.array([40, 60, 80, 100])
+    T_max_array = np.array(T_max_list)
+    for i in range(len(T_max_list)):
+        plt.plot(1./float(L[i]), T_max_list[i], "o", label = "")
+
+    x = 1./L_array
+    y = T_max_array
+    m, b = np.polyfit(x, y, 1)
+    slope, intercept, r, p, se = linregress(x, y)
+    Tc = "{0:.3f}".format(intercept)
+    plt.plot(x, m*x + b, label = "T$_c$ = " + str(Tc))
+    plt.title("Estimate of T$_c$ using suceptibility", size = 22)
+    plt.xlabel("Inverce lattice size", size = 20)
+    plt.ylabel("Critical temperature/[J/$k_B$]", size = 20)
+    plt.xticks(size = 17)
+    plt.yticks(size = 17)
+    plt.legend(fontsize = 17)
+    plt.show()
