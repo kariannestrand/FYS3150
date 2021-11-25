82fb2ec6650f1d2683bacc38d18ecad28f58c8aa
kariannestrand <kariannestrand@outlook.com>
2021-11-25 15:19:02 +0100

    solver

--- Project_5/functions.cpp
+++ Project_5/functions.cpp
@@ -3,54 +3,73 @@
 using namespace arma;
 using namespace std;
 
-
+/*
 cx_vec state(cx_mat U_in){
     cx_vec u = U_in.as_col();
     return u;
 }
+*/
+
+
+void vector_ab(double r, double dt, int M, cx_vec &a, cx_vec &b){
+    cx_mat V = cx_mat((M-2), (M-2), fill::ones);
+    cx_vec v = V.as_col();
+    complex<double> i(0.0, 1.0);
+
+    for (int k = 0; k < pow(M-2, 2); k++){
+        a(k) = 1 + 4*r + i*dt/2.0 * v(k);
+        b(k) = 1 - 4*r - i*dt/2.0 * v(k);
+    }
+}
+
 
 void matrix(double r, cx_vec a, cx_vec b, cx_mat &A, cx_mat &B, int M){
-    // A matrix
     for (int i = 0; i < (M-2)*(M-2); i++){
         A(i, i) = a(i);
-    }
-    for (int i = 0; i < (M-2)*(M-2)-(M-2); i++){
-    // filling sub- and superdiagonal with a:
-        A(i, i + (M-2)) = -r;
-        A(i + (M-2), i) = -r;
-    }
-    for (int i = 0; i < (M-2)*(M-2)-1; i++){
-        A(i, i + 1) = -r;
-        A(i + 1, i) = -r;
-        for (int j = 1; j < (M-2)*(M-2)-1; j++){
-            if (i == j*(M-2) - 1){
-                A(i, i + 1) = 0;
-                A(i + 1, i) = 0;
-            }
-        }
-    }
-
-
-    // B matrix
-    for (int i = 0; i < (M-2)*(M-2); i++){
         B(i, i) = b(i);
     }
     for (int i = 0; i < (M-2)*(M-2)-(M-2); i++){
-    // filling sub- and superdiagonal with a:
+        A(i, i + (M-2)) = -r;
+        A(i + (M-2), i) = -r;
         B(i, i + (M-2)) = r;
         B(i + (M-2), i) = r;
     }
     for (int i = 0; i < (M-2)*(M-2)-1; i++){
+        A(i, i + 1) = -r;
+        A(i + 1, i) = -r;
         B(i, i + 1) = r;
         B(i + 1, i) = r;
         for (int j = 1; j < (M-2)*(M-2)-1; j++){
             if (i == j*(M-2) - 1){
+                A(i, i + 1) = 0;
+                A(i + 1, i) = 0;
                 B(i, i + 1) = 0;
                 B(i + 1, i) = 0;
             }
         }
     }
+}
 
 
+void solver(cx_mat U_in, cx_mat B){
+    cx_vec u = U_in.as_col();
+    cx_vec b = cx_vec(u.size());
 
+    /*
+    for (int k = 0; k < u.size(); k++){
+        cx_double B_tot = 0;
+        for (int s = 0; s < u.size(); s++){
+            B_tot += B.col(k)(s);
+        }
+        b(k) = B_tot*u(k);
+    }
+    */
+
+    /*
+    for (int k = 0; k < u.size(); k++){
+        b(k) = B.each_col() %= u(k);
+    }
+    */
+
+    cout << b << endl;
 }
--- Project_5/main.cpp
+++ Project_5/main.cpp
@@ -8,20 +8,22 @@ int main(int argc, char const *argv[]){
     double h = 0.1;         // step size in x and y direction
     double dt = 0.1;        // step size for t
     int T = 100;            // total time
+    double r = 2;
 
     cx_mat U_in = cx_mat(M-2, M-2, fill::randu);
-    cx_vec u = state(U_in);
 
+    cx_vec a = cx_vec((M-2)*(M-2));
+    cx_vec b = cx_vec((M-2)*(M-2));
 
     cx_mat A = cx_mat((M-2)*(M-2), (M-2)*(M-2), fill::zeros);
     cx_mat B = cx_mat((M-2)*(M-2), (M-2)*(M-2), fill::zeros);
 
-    cx_vec a = cx_vec((M-2)*(M-2), fill::randu);
-    cx_vec b = cx_vec((M-2)*(M-2), fill::randu);
-    double r = 2;
-
+    vector_ab(r, dt, M, a, b);
     matrix(r, a, b, A, B, M);
-    cout << B << endl;
+
+    solver(U_in, B);
+
+
 
 
 
