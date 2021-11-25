64213815bd72cb748d7c9dc91b023c6b73261adb
kariannestrand <kariannestrand@outlook.com>
2021-11-25 10:25:58 +0100

    helo!

--- Project_5/main.cpp
+++ Project_5/main.cpp
@@ -4,10 +4,10 @@ using namespace arma;
 using namespace std;
 
 int main(int argc, char const *argv[]){
-    int M = 5;      // size of one side of outer matrix
-    double h = 0.1;    // step size in x and y direction
-    double dt = 0.1;    // step size for t
-    int T = 100;        // total time
+    int M = 5;              // size of one side of outer matrix
+    double h = 0.1;         // step size in x and y direction
+    double dt = 0.1;        // step size for t
+    int T = 100;            // total time
 
 
     return 0;
