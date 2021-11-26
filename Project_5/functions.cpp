2fbd238a25a1faca00a1b9dff6b0e51ae75dc84f
kariannestrand <kariannestrand@outlook.com>
2021-11-26 12:58:29 +0100

    nicolson

--- Project_5/functions.cpp
+++ Project_5/functions.cpp
@@ -10,24 +10,51 @@ cx_mat initial(double mean_x, double mean_y, double var_x, double var_y, int M){
 
     cx_vec distr_x = cx_vec(M-2);
     cx_vec distr_y = cx_vec(M-2);
-    cx_mat u = cx_mat(M-2, M-2);
+    cx_mat U_in = cx_mat(M-2, M-2);
 
+    complex<double> i(0.0, 1.0);
+    double k_x = 1;
+    double k_y = 1;
     for (int i = 0; i < M-2; i++){
-        distr_x(i) = 1/(var_x*sqrt(2*M_PI))*exp(-(x(i) - mean_x)/(var_x)*(x(i) - mean_x)/(var_x));
-        distr_y(i) = 1/(var_y*sqrt(2*M_PI))*exp(-(y(i) - mean_y)/(var_y)*(y(i) - mean_y)/(var_y));
+        distr_x(i) = 1/(var_x*sqrt(2*M_PI))*exp(-(x(i) - mean_x)/(var_x)*(x(i) - mean_x)/(var_x))*exp(i*k_x*x(i));
+        distr_y(i) = 1/(var_y*sqrt(2*M_PI))*exp(-(y(i) - mean_y)/(var_y)*(y(i) - mean_y)/(var_y))*exp(i*k_y*y(i));
     }
 
     for (int i = 0; i < M-2; i++){
         for (int j = 0; j < M-2; j++){
-            u(i, j) = distr_x(i)*distr_y(j);
+            U_in(i, j) = distr_x(i)*distr_y(j);
         }
     }
 
-    return u;
+    U_in = U_in/norm(U_in);  // making sure probability distribution starts out normalized to 1
+
+    return U_in;
 }
 
-void vector_ab(double r, double dt, int M, cx_vec &a, cx_vec &b){
-    cx_mat V = cx_mat((M-2), (M-2), fill::ones);
+
+cx_mat potential(double v0, int M, int size_slit, int size_between_slit){
+    cx_mat V = cx_mat(M-2, M-2, fill::zeros);
+    for (int i = 0; i < M-2; i++){
+        for (int j = 0; j < M-2; j++){
+            if (j == (M-2)/2){
+                V(i, j) = v0;
+
+                for (int k = size_between_slit; k < size_between_slit + size_slit; k++){
+                    if (i == (M-2)/2 + k){
+                        V(i, j) = 0.0;
+                    }
+                    else if (i == (M-2)/2 - k){
+                        V(i, j) = 0.0;
+                    }
+                }
+            }
+        }
+    }
+    return V;
+}
+
+
+void vector_ab(double r, double dt, int M, cx_vec &a, cx_vec &b, cx_mat V){
     cx_vec v = V.as_col();
     complex<double> i(0.0, 1.0);
 
@@ -36,6 +63,7 @@ void vector_ab(double r, double dt, int M, cx_vec &a, cx_vec &b){
         b(k) = 1 - 4*r - i*dt/2.0 * v(k);
     }
 }
+
 
 void matrix(double r, cx_vec a, cx_vec b, sp_cx_mat &A, cx_mat &B, int M){
     for (int i = 0; i < (M-2)*(M-2); i++){
@@ -64,9 +92,14 @@ void matrix(double r, cx_vec a, cx_vec b, sp_cx_mat &A, cx_mat &B, int M){
     }
 }
 
-cx_vec solver(cx_mat U_in, cx_mat B, sp_cx_mat A){
+
+cx_vec CrankNicolson(cx_mat U_in, cx_mat B, sp_cx_mat A, double t){
     cx_vec u = U_in.as_col();
     cx_vec b = cx_vec(u.size());
-    b = B*u;
-    return spsolve(A, b);
+
+    for (int i = 0; i < t; i++){
+        b = B*u;
+        u = spsolve(A, b);
+    }
+    return u;
 }
--- Project_5/main.cpp
+++ Project_5/main.cpp
@@ -4,11 +4,14 @@ using namespace arma;
 using namespace std;
 
 int main(int argc, char const *argv[]){
-    int M = 5;              // size of one side of outer matrix
-    double h = 0.1;         // step size in x and y direction
-    double dt = 0.1;        // step size for t
-    int T = 100;            // total time
+    int M = 10;               // size of one side of outer matrix
+    double h = 0.1;           // step size in x and y direction
+    double dt = 0.1;          // step size for t
+    double t = 100;           // total time
     double r = 2;
+    double v0 = 10000;
+    int size_slit = 1;
+    int size_between_slit = 1;
 
     cx_vec a = cx_vec((M-2)*(M-2));
     cx_vec b = cx_vec((M-2)*(M-2));
@@ -20,10 +23,14 @@ int main(int argc, char const *argv[]){
     double var_x = 0.25;
     double var_y = 0.25;
 
-    cx_mat U_in = initial(mean_x, mean_y, var_x, var_y, M);   // creates u(0, 0, 0) matrix
-    vector_ab(r, dt, M, a, b);                                // creating a and b vector
-    matrix(r, a, b, A, B, M);                                 // creating A and B matrix
-    cx_vec U_in_vec = solver(U_in, B, A);                     // solving u(0, 0, n+1)
+    cx_mat U_in = initial(mean_x, mean_y, var_x, var_y, M);      // creates u(0, 0, 0) matrix
+    cx_mat V = potential(v0, M, size_slit, size_between_slit);
+    vector_ab(r, dt, M, a, b, V);                                // creating a and b vector
+    matrix(r, a, b, A, B, M);                                    // creating A and B matrix
 
+
+    cx_vec U_in_vec = CrankNicolson(U_in, B, A, t);              // solving u(0, 0, 1)
+
+    cout << U_in_vec << endl;
     return 0;
 }
